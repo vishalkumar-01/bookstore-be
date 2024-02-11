dd27ef91212aca4c6fdd16fc15db6241e49e9013
vishalkumar-01 <124910291+vishalkumar-01@users.noreply.github.com>
2024-02-11 10:03:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const port=5500;
-export const mongodb='mongodb://127.0.0.1:27017/bookmodel'
-export const mongo='mongodb+srv://vishalsure1:Vishal-1@bookstore.ylomcky.mongodb.net/bookcollection?retryWrites=true&w=majority';
+export const mongo='mongodb://127.0.0.1:27017/bookmodel'
+export const mongodb='mongodb+srv://vishalsure1:Vishal-1@bookstore.ylomcky.mongodb.net/bookcollection?retryWrites=true&w=majority';
